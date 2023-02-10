c09baf74cdb08d9261312b80e9454ae709c01132
Xavier Noria <fxn@hashref.com>
2023-02-10 06:24:42 +0100

    Make the private namespace_dirs internal

--- lib/im/loader.rb
+++ lib/im/loader.rb
@@ -84,9 +84,9 @@ module Im
     # and that its children are spread over those directories. We'll visit them
     # to set up the corresponding autoloads.
     #
-    # @private
     # @sig Hash[String, Array[String]]
     attr_reader :namespace_dirs
+    internal :namespace_dirs
 
     # A shadowed file is a file managed by this loader that is ignored when
     # setting autoloads because its matching constant is already taken.
