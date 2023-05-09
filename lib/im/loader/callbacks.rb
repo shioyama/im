fxn/zeitwerk

Log failed autoloads whose require was successful
If the user rescues NameError and the logger is printing traces,
a failed autoload could be missed in a debugging session.

lib/zeitwerk/loader/callbacks.rb
@@ -22,7 +22,9 @@ module Zeitwerk::Loader::Callbacks
       log("constant #{cpath} loaded from file #{file}") if logger
       run_on_load_callbacks(cpath, cget(*cref), file) unless on_load_callbacks.empty?
     else
-      raise Zeitwerk::NameError.new("expected file #{file} to define constant #{cpath}, but didn't", cref.last)
+      msg = "expected file #{file} to define constant #{cpath}, but didn't"
+      log(msg) if logger
+      raise Zeitwerk::NameError.new(msg, cref.last)
     end
   end
 

shioyama/im

lib/im/loader/callbacks.rb
@@ -21,7 +21,9 @@ module Im::Loader::Callbacks
       log("constant #{relative_cpath} loaded from file #{file}") if logger
       run_on_load_callbacks(relative_cpath, obj, file) unless on_load_callbacks.empty?
     else
-      raise Im::NameError.new("expected file #{file} to define constant #{cpath(*cref)}, but didn't", cref.last)
+      msg = "expected file #{file} to define constant #{cpath(*cref)}, but didn't"
+      log(msg) if logger
+      raise Im::NameError.new(msg, cref.last)
     end
   end
 