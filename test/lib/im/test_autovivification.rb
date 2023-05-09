fxn/zeitwerk

Adds a test that verifies internal state consistency for namespaces

test/lib/zeitwerk/test_autovivification.rb
@@ -57,6 +57,16 @@ class TestAutovivification < LoaderTest
     end
   end
 
+  test "does not register the namespace as explicit" do
+    files = [
+      ["rd1/admin/x.rb", "Admin::X = true"],
+      ["rd2/admin/y.rb", "Admin::Y = true"]
+    ]
+    with_setup(files) do
+      assert Zeitwerk::ExplicitNamespace.send(:cpaths).empty?
+    end
+  end
+
   test "autovivification is synchronized" do
     $test_admin_const_set_queue = Queue.new
 

shioyama/im

test/lib/im/test_autovivification.rb
@@ -29,6 +29,16 @@ class TestAutovivification < LoaderTest
     end
   end
 
+  test "does not register the namespace as explicit" do
+    files = [
+      ["rd1/admin/x.rb", "Admin::X = true"],
+      ["rd2/admin/y.rb", "Admin::Y = true"]
+    ]
+    with_setup(files) do
+      assert Im::ExplicitNamespace.send(:cpaths).empty?
+    end
+  end
+
   test "autovivification is synchronized" do
     $test_admin_const_set_queue = Queue.new
 