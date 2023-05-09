fxn/zeitwerk

Reset module state on `Zeitwerk::NameError`
If an autoload is triggered, the file is loaded successfully, but the expected
constant does not get defined, Ruby resets the state of the module. In
particular, `autoload?` returns `nil` for that constant name, and `constants`
does not include the constant name (starting with Ruby 3.1).

Zeitwerk is more strict, not defining the expected constant is an error
condition and the loader raises `Zeitwerk::NameError`. But this happens during
the `require` call and the exception prevents Ruby from doing that cleanup.

This does not matter if the exception aborts the program. However, if the user
rescues the error, the module is left in a state that makes more sense.

lib/zeitwerk/loader/callbacks.rb
@@ -11,19 +11,17 @@ module Zeitwerk::Loader::Callbacks
     cref  = autoloads.delete(file)
     cpath = cpath(*cref)
 
-    # If reloading is enabled, we need to put this constant for unloading
-    # regardless of what cdef? says. In Ruby < 3.1 the internal state is not
-    # fully cleared. Module#constants still includes it, and you need to
-    # remove_const. See https://github.com/ruby/ruby/pull/4715.
-    to_unload[cpath] = [file, cref] if reloading_enabled?
     Zeitwerk::Registry.unregister_autoload(file)
 
     if cdef?(*cref)
       log("constant #{cpath} loaded from file #{file}") if logger
+      to_unload[cpath] = [file, cref] if reloading_enabled?
       run_on_load_callbacks(cpath, cget(*cref), file) unless on_load_callbacks.empty?
     else
       msg = "expected file #{file} to define constant #{cpath}, but didn't"
       log(msg) if logger
+      crem(*cref)
+      to_unload[cpath] = [file, cref] if reloading_enabled?
       raise Zeitwerk::NameError.new(msg, cref.last)
     end
   end

test/lib/zeitwerk/test_reloading.rb
@@ -205,8 +205,16 @@ class TestReloading < LoaderTest
     with_setup(files) do
       assert_raises(Zeitwerk::NameError) { X }
 
+      assert !Object.constants.include?(:X)
+      assert !Object.const_defined?(:X, false)
+      assert !Object.autoload?(:X)
+
       loader.reload
       File.write("x.rb", "X = true")
+
+      assert Object.constants.include?(:X)
+      assert Object.const_defined?(:X, false)
+      assert Object.autoload?(:X)
 
       assert X
     end
@@ -220,8 +228,16 @@ class TestReloading < LoaderTest
       loader.on_unload {}
       assert_raises(Zeitwerk::NameError) { X }
 
+      assert !Object.constants.include?(:X)
+      assert !Object.const_defined?(:X, false)
+      assert !Object.autoload?(:X)
+
       loader.reload
       File.write("x.rb", "X = true")
+
+      assert Object.constants.include?(:X)
+      assert Object.const_defined?(:X, false)
+      assert Object.autoload?(:X)
 
       assert X
     end

shioyama/im

lib/im/loader/callbacks.rb
@@ -9,7 +9,6 @@ module Im::Loader::Callbacks
     cref = autoloads.delete(file)
 
     relative_cpath = relative_cpath(*cref)
-    to_unload[relative_cpath] = [file, cref] if reloading_enabled?
     Im::Registry.unregister_autoload(file)
 
     if cdef?(*cref)
@@ -19,10 +18,13 @@ module Im::Loader::Callbacks
         Im::Registry.register_autoloaded_module(get_object_hash(obj), relative_cpath, self)
       end
       log("constant #{relative_cpath} loaded from file #{file}") if logger
+      to_unload[relative_cpath] = [file, cref] if reloading_enabled?
       run_on_load_callbacks(relative_cpath, obj, file) unless on_load_callbacks.empty?
     else
       msg = "expected file #{file} to define constant #{cpath(*cref)}, but didn't"
       log(msg) if logger
+      crem(*cref)
+      to_unload[relative_cpath] = [file, cref] if reloading_enabled?
       raise Im::NameError.new(msg, cref.last)
     end
   end

test/lib/im/test_reloading.rb
@@ -157,8 +157,16 @@ class TestReloading < LoaderTest
     with_setup(files) do
       assert_raises(Im::NameError) { loader::X }
 
+      assert !loader.constants.include?(:X)
+      assert !loader.const_defined?(:X, false)
+      assert !loader.autoload?(:X)
+
       loader.reload
       File.write("x.rb", "X = true")
+
+      assert loader.constants.include?(:X)
+      assert loader.const_defined?(:X, false)
+      assert loader.autoload?(:X)
 
       assert loader::X
     end
@@ -170,8 +178,16 @@ class TestReloading < LoaderTest
       loader.on_unload {}
       assert_raises(Im::NameError) { loader::X }
 
+      assert !loader.constants.include?(:X)
+      assert !loader.const_defined?(:X, false)
+      assert !loader.autoload?(:X)
+
       loader.reload
       File.write("x.rb", "X = true")
+
+      assert loader.constants.include?(:X)
+      assert loader.const_defined?(:X, false)
+      assert loader.autoload?(:X)
 
       assert loader::X
     end