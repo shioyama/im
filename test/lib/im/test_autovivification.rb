fxn/zeitwerk

Introduce an internal method for better encapsulation towards the test suite

test/lib/zeitwerk/test_autovivification.rb
@@ -63,7 +63,7 @@ class TestAutovivification < LoaderTest
       ["rd2/admin/y.rb", "Admin::Y = true"]
     ]
     with_setup(files) do
-      assert Zeitwerk::ExplicitNamespace.send(:cpaths).empty?
+      assert !Zeitwerk::ExplicitNamespace.__registered?("Admin")
     end
   end
 

shioyama/im

test/lib/im/test_autovivification.rb
@@ -35,7 +35,7 @@ class TestAutovivification < LoaderTest
       ["rd2/admin/y.rb", "Admin::Y = true"]
     ]
     with_setup(files) do
-      assert Im::ExplicitNamespace.send(:cpaths).empty?
+      assert !Im::ExplicitNamespace.__registered?("Admin")
     end
   end
 